52ab198d0742155083c233253f1cc4dcc7c9dc12
BRISSETLo <leo.brisset@etu.univ-orleans.fr>
2023-12-21 15:40:53 +0100

    Modification du code pour utiliser Scapy

--- partie3/partie3.py
+++ partie3/partie3.py
@@ -1,30 +1,28 @@
-import dpkt
 from Crypto.Cipher import AES
-from Crypto.Util.Padding import unpad
+from scapy.all import IP, UDP, Raw, rdpcap
 import main
 
+def pkcs7_unpad(data):
+    padding_size = data[-1]
+
+    if all(i == padding_size for i in data[-padding_size:]):
+        return data[:-padding_size]
+    else:
+        return data
 
 def encrypt_messages(trace_path):
     encrypted_messages = []
+    packets = rdpcap(trace_path)
 
-    with open(trace_path, 'rb') as file:
-        pcap = dpkt.pcap.Reader(file)
+    for packet in packets:
+        if IP in packet and UDP in packet[IP] and packet[IP].dport == 9999:
+            iv_and_message = bytes(packet[Raw].load)
+            iv = iv_and_message[:16]
+            encrypted_message = iv_and_message[16:]
 
-        for _, packet in pcap:
-            eth = dpkt.ethernet.Ethernet(packet)
-
-            if isinstance(eth.data, dpkt.ip.IP) and isinstance(
-                    eth.data.data, dpkt.udp.UDP):
-                udp = eth.data.data
-                if udp.dport == 9999:
-                    iv_and_message = udp.data
-                    iv = iv_and_message[:16]
-                    encrypted_message = iv_and_message[16:]
-
-                    encrypted_messages.append((iv, encrypted_message))
+            encrypted_messages.append((iv, encrypted_message))
 
     return encrypted_messages
-
 
 def decrypt_messages(key, encrypted_messages):
     decrypted_messages = []
@@ -32,15 +30,13 @@ def decrypt_messages(key, encrypted_messages):
     for iv, encrypted_message in encrypted_messages:
         cipher = AES.new(key, AES.MODE_CBC, iv=iv)
         padded_message = cipher.decrypt(encrypted_message)
-        message = unpad(padded_message, AES.block_size)
+        message = pkcs7_unpad(padded_message)
 
         decrypted_messages.append(message)
 
     return decrypted_messages
 
-
 if __name__ == "__main__":
-
     rossignol2 = main.open_image("./docs/rossignol2.bmp")
 
     original_key = ""
@@ -52,8 +48,7 @@ if __name__ == "__main__":
     for i in range(2):
         original_key += original_key
 
-    key = int(original_key, 2).to_bytes(len(original_key) // 8,
-                                        byteorder='big')
+    key = int(original_key, 2).to_bytes(len(original_key) // 8, byteorder='big')
 
     encrypted_messages = encrypt_messages("./docs/trace_sae.cap")
     print(encrypted_messages)
